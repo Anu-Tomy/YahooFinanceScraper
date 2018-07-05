632b3c4a2efc8cf3e8644a0792ea07b1022afa40
Anu Tomy <anuatomy@gmail.com>
2018-07-05 10:42:11 -0400

    update

--- src/practice/Stock.java
+++ src/practice/Stock.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class Stock {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		String projectLocation = System.getProperty("user.dir");
 		
