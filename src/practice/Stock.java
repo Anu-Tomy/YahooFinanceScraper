6b2bb2864af742bd6adfb2a73b363098cc5ec534
Anu Tomy <anuatomy@gmail.com>
2018-07-05 23:18:53 -0400

    Create Stocks list

--- src/practice/Stock.java
+++ src/practice/Stock.java
@@ -1,6 +1,10 @@
 package practice;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class Stock {
@@ -16,8 +20,20 @@ public class Stock {
 		driver.get("https://finance.yahoo.com/screener/predefined/most_actives?offset=0&count=50");
 		
 		//wait
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		
+		System.out.println(driver.getTitle());
+		
+		String xpath = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]";  
+		
+				
+		List<WebElement> tableRows = driver.findElements(By.xpath(xpath));
+		
+		for(int i = 0; i <= 50; i++) {
+			
+			System.out.println((i + 1) + " - " + tableRows.get(i).getText());
+		}		
+				
 		driver.close();
 		
 	}
